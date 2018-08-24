c67534800b929263c86b2fa36992fecdc5067210
El hadji Diagne <elma.diagne@elmadeal.com>
2018-08-24 11:44:09 +0100

    mister Aj

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.all
+  end
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.valid?
+      @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+    render :new
+  end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+    def dose_params
+     params.require(:dose).permit(:description, :ingredient_id)
+    end
+end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,9 @@
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
 // Bootstrap 3 has a dependency over jQuery:
-const webpack = require('webpack')
+const webpack = require('webpack');
+
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
